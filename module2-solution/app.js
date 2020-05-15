41b1932dfa003cd20b9058fbfd1b9b568e8f04ab
Mark Mounts <mark.mounts@dartmouth.edu>
2020-05-15 11:46:24 -0400

    save new stuff

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,6 +1,45 @@
 (function () {
     'use strict';
 
-    angular.module(ShoppingListCheckOff, []);
+    angular.module(ShoppingListCheckOff, [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+
+    }
+
+    function ShoppingListCheckOffService() {
+        var items = [
+            { 
+                name: "cookies", 
+                quantity: 5 
+            },
+            {
+                name: "oranges",
+                quantity: 3
+            },
+            {
+                name: "chips",
+                quantity: 2
+            },
+            {
+                name: "milk",
+                quantity: 4
+            },
+            {
+                name: "soda",
+                quantity: 7
+            }
+        ];
+        var purchased = [];
+    }
 
 })();
